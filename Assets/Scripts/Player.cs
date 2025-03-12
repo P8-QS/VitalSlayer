37bbe11ee7b4c1a5fe3ed35491b73bc6a009c8c6
Bergulff <Bergulff@hotmail.com>
2025-03-12 14:41:11 +0100

    initialized player attributions, need modifications

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,11 @@ public class Player : MonoBehaviour
     private BoxCollider2D boxCollider;
     private JoystickMove joystickMove;
     private Vector3 initialSize;
+
+    public string playerName;
+    public int playerHealth;
+    public int playerAttack;
+    public int xp;
     
 
 
@@ -13,6 +18,11 @@ public class Player : MonoBehaviour
         boxCollider = GetComponent<BoxCollider2D>();
         joystickMove = GetComponent<JoystickMove>();
         initialSize = transform.localScale;
+
+        // player attributes
+        playerHealth = 100;
+        playerAttack = 10;
+        xp = 0;
     }
 
     private void FixedUpdate()
@@ -27,6 +37,27 @@ public class Player : MonoBehaviour
         {
             transform.localScale = new Vector3(initialSize.x, initialSize.y, initialSize.z);
         }
+    }
+
+    public void SetPlayerName(string name)
+    {
+        playerName = name;
+    }
+    
+    public void TakeDmg(int dmg){
+        playerHealth -= dmg;
+        if(playerHealth <= 0){
+            playerHealth = 0;
+           // Debug.Log("Player is dead");
+        }
+    }
+
+    public void GainXp(int xpAmount){
+        xp += xpAmount;
+    }
+
+    public void Attack(Player target){
+        target.TakeDmg(playerAttack);
     }
     
 }
