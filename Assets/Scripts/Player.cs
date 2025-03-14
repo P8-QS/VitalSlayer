df9527a37e70aa7c24a35c15f8d9dc9a84f0649c
Bergulff <Bergulff@hotmail.com>
2025-03-14 10:48:02 +0100

    small changes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,7 @@ public class Player : MonoBehaviour
     public float playerHealth;
     public float playerAttack;
     public float xp = 0.0f;
-    public int level = 1;
+    public int level;
 
     public float xPToNextLevel;
     // Dunno how to handle "bot enemies" yet
@@ -25,12 +25,12 @@ public class Player : MonoBehaviour
         initialSize = transform.localScale;
 
         // player attributes
-        playerName = "Svin";
-        level = 1;
-        playerHealth = SetPlayerHealth();//(float) Math.Floor(100 + (25 * Math.Pow(level, 1.2)));//
-        playerAttack = SetPlayerAttack(); //(float) Math.Floor(10 + (4 * Math.Pow(level, 1.4))); //setPlayerAttack()
+        playerName = SetPlayerName("Svin");
+        level = 2;
+        playerHealth = SetPlayerHealth();
+        playerAttack = SetPlayerAttack();
         xp = 0;
-        xPToNextLevel = (float) Math.Floor(100 * Math.Pow(level, 1.5));
+        xPToNextLevel = GetNextLevel(); 
     }
 
     private void FixedUpdate()
@@ -46,7 +46,11 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(initialSize.x, initialSize.y, initialSize.z);
         }
     }
-    
+
+    public int GetLevel()
+    {
+        return level;
+    }
     // Update is called once per frame, but we only want to update the player's stats every second
     private void Update()
     {
@@ -57,70 +61,53 @@ public class Player : MonoBehaviour
             Debug.Log("Update");
             
             // update player level, xp and name such that when player leaves the game and comes back, the player will still have the same level, xp and name
-             PlayerPrefs.SetString("PlayerName", playerName);
-             // debug log player name
-             // Debug.Log(playerName);
-             /*
-             PlayerPrefs.SetFloat("PlayerHealth", (float)playerHealth);
-             PlayerPrefs.SetFloat("PlayerAttack", playerAttack);
-             PlayerPrefs.SetInt("PlayerLevel", level);
-             PlayerPrefs.SetFloat("PlayerXP", xp); */
+            PlayerPrefs.SetString("PlayerName", playerName);
+             // PlayerPrefs.SetFloat("PlayerHealth", playerHealth);
+             // PlayerPrefs.SetFloat("PlayerAttack", playerAttack);
+             // PlayerPrefs.SetInt("PlayerLevel", level);
+             // PlayerPrefs.SetFloat("PlayerXP", xp);
         }
     
     }
     
-    /* public void TakeDmg(int dmg){
-        playerHealth -= dmg;
-        if(playerHealth <= 0){
-            playerHealth = 0;
-           // Debug.Log("Player is dead");
-        }
-    } */
-
-    public void SetPlayerName(string name)
-    {
+    
+    private string SetPlayerName(string name){
         playerName = name;
+        return playerName;
     }
     
-
+    private float SetPlayerAttack(){
+        // dunno if attack should be ints our doubles like 20.1 or just 20
+        return (float) Math.Floor(10 + (4 * Math.Pow(level, 1.4)));
+    }
     private float SetPlayerHealth(){
       return (float)Math.Floor(100 + (25 *  Math.Pow(level, 1.2)));
     }
-
     public void GainXpByFighting(Enemy enemy)
     {
-        double enemyXP = 10 * Math.Pow(enemy.level, 2);
-        double bossXP = 30 * Math.Pow(enemy.level, 2);
+        double enemyXp = 10 * Math.Pow(enemy.level, 2);
+        double bossXp = 30 * Math.Pow(enemy.level, 2);
 
         if (enemy == boss)
         {
-            xp += (int)bossXP;
+            xp += (int)bossXp;
         }
         else
         {
-            xp += (int)enemyXP;
+            xp += (int)enemyXp;
         }
     }
-
     public void QuestXp(){
         // quest difficulty calcluation = 2 + (player.level * 0.8)
         float questDifficulty = (float) (2 + (level * 0.8));
         float questExperience = 50 + (20 + questDifficulty);
         xp += questExperience;
     }
-    public float SetPlayerAttack(){
-        // dunno if attack should be ints our doubles like 20.1 or just 20
-       return (float) Math.Floor(10 + (4 * Math.Pow(level, 1.4)));
-    }
-    
     public float GetNextLevel()
     {
         xPToNextLevel = (float)Math.Floor(100 * Math.Pow(level, 1.5));
-        Debug.Log("XP to Next Level Calculated: " + xPToNextLevel);
         return xPToNextLevel; 
     }
-
-    
     public void LevelUp()
     {
         float nextLevel = GetNextLevel();
@@ -130,9 +117,7 @@ public class Player : MonoBehaviour
             playerHealth += (float)SetPlayerHealth();
             playerAttack += (float)SetPlayerAttack();
         }
-        
     }
-    
 }
 
 
--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
 {
-    private Player _player; // Reference to the _player script
+    private Player _player; 
     public Text playerNameTxt;
     public Text level;
     public Text xp;
@@ -15,7 +15,6 @@ public class PlayerUI : MonoBehaviour
     private void Start()
     {
         // Find the _player script in the scene
-        //_player = FindObjectOfType<_player>();
         _player = FindFirstObjectByType<Player>();
 
         // Make sure we found a _player before continuing
@@ -24,8 +23,6 @@ public class PlayerUI : MonoBehaviour
             Debug.LogError("_playerUI: No _player script found in the scene!");
             return;
         }
-
-        // Initialize UI values
         UpdateUI();
     }
 
@@ -34,15 +31,15 @@ public class PlayerUI : MonoBehaviour
         if (_player)
         {
             UpdateUI();
-            //Update_playerNameUI();
         }
     }
 
     private void UpdateUI()
     {
-        if (_player == null) return; // Prevent null errors
+        if (_player == null) return; 
 
         playerNameTxt.text = _player.playerName;
+        level.text = "Level: " + _player.GetLevel();
         level.text = "Level: " + _player.level;
         xp.text = "XP: " + _player.xp;
         playerHealth.text = "Health: " + _player.playerHealth;
@@ -50,15 +47,4 @@ public class PlayerUI : MonoBehaviour
         xpToNextLevel.text = "XP to next level: " + _player.xPToNextLevel;
        
     }
-
-    // private void Update_playerNameUI()
-    // {
-    //     if (_player == null) return;
-    //     
-    //     _playerNameTxt.text = "_player: SVIN " + _player._playerName;
-    //     
-    //     Debug.LogError("_playerUI: _playerNameText UI element not assigned!");
-    // }
-    
-    
 }
