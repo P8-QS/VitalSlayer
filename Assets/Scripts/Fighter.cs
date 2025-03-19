a92d33c99d8da1257e13f72d6aab4df20e5efbd9
Bergulff <Bergulff@hotmail.com>
2025-03-19 11:23:06 +0100

    Weapon swing animation

--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -6,6 +6,7 @@ public class Fighter : MonoBehaviour
     public int hitpoint = 10;
     public int maxHitpoint = 10;
     public float pushRecoverySpeed = 0.2f;
+    public int currentLevel = 1;
 
     // Immunity
     private float immuneTime = 1.0f;
