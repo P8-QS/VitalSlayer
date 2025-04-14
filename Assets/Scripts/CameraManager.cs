6ee4702c8d199e7d5b045aa794d0c385dc7c20b2
Viktor Platz <viktorplatz@outlook.com>
2025-04-14 13:55:51 +0200

    Scuffed way of making sure we switch cameras based on rooms and fixing room boundary issue

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,16 +1,61 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraManager : MonoBehaviour
 {
+    private Tilemap currentTilemap;
+    private CinemachineCamera currentCamera;
+    private float lastSwitchTime = 0f;
+    private float switchCooldown = 0.3f;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void Update()
     {
-        if (!other.CompareTag("Room")) return;
-        var cam = other.GetComponentInChildren<CinemachineCamera>();
-        if (cam == null) return;
-        SetActiveCamera(cam);
-        cam.Follow = transform;
+        Vector3 playerPosition = transform.position;
+
+        Tilemap playerTilemap = GetTilemapAtPosition(playerPosition);
+
+        // If we found a tilemap and it's different from the current one
+        if (playerTilemap != null && playerTilemap != currentTilemap)
+        {
+            // Check cooldown
+            if (Time.time - lastSwitchTime < switchCooldown)
+                return;
+
+            // Get the room parent
+            Transform roomTransform = playerTilemap.transform.parent;
+            if (roomTransform != null)
+            {
+                // Find the camera in this room
+                CinemachineCamera roomCamera = roomTransform.GetComponentInChildren<CinemachineCamera>();
+                if (roomCamera != null && roomCamera != currentCamera)
+                {
+                    // Switch camera
+                    currentCamera = roomCamera;
+                    currentTilemap = playerTilemap;
+                    SetActiveCamera(roomCamera);
+                    roomCamera.Follow = transform;
+                    lastSwitchTime = Time.time;
+                }
+            }
+        }
+    }
+
+    private Tilemap GetTilemapAtPosition(Vector3 position)
+    {
+        Tilemap[] tilemaps = FindObjectsByType<Tilemap>(FindObjectsSortMode.None);
+
+        foreach (Tilemap tilemap in tilemaps)
+        {
+            Vector3Int cellPosition = tilemap.WorldToCell(position);
+
+            if (tilemap.HasTile(cellPosition))
+            {
+                return tilemap;
+            }
+        }
+
+        return null;
     }
 
     private void SetActiveCamera(CinemachineCamera activeCam)
