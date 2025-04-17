646d6dc41ff868a95884164f591780622d1317e5
sabotack <sabotack.2001@gmail.com>
2025-04-17 23:50:54 +0200

    A bit more cleanup

--- Assets/Scripts/TrapManager.cs
+++ Assets/Scripts/TrapManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations;
 using UnityEngine.Tilemaps;
 
 public class TrapManager : MonoBehaviour
