53ab51bd0b2ef4494a15961cee948e06b9261292
sabotack <sabotack.2001@gmail.com>
2025-04-25 14:52:12 +0200

    ExerciseMetric completion

--- Assets/Scripts/Metrics/ExerciseMetric.cs
+++ Assets/Scripts/Metrics/ExerciseMetric.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
+using Data;
+using Data.Models;
 using Effects;
 using UnityEngine;
 
@@ -5,23 +9,40 @@ namespace Metrics
 {
     public class ExerciseMetric : IMetric
     {
+        public IReadOnlyCollection<ExerciseSessionRecord> Data { get; }
         public string Name => "Exercise";
         public IEffect Effect { get; }
         public Sprite Icon { get; }
      
         public ExerciseMetric()
         {
-            throw new System.NotImplementedException();
+            Data = UserMetricsHandler.Instance.ExerciseSessionRecords;
+            Icon = SpriteManager.Instance.GetSprite("metric_exercise");
+
+            if (Data.Count > 0)
+            {
+                var effectLevel = Data.Sum(exercise => exercise.Duration.TotalMinutes) switch
+                {
+                    > 30 => 2,
+                    > 1 => 1,
+                    _ => 0
+                };
+
+                if (effectLevel > 0)
+                {
+                    Effect = new CriticalChanceEffect(SpriteManager.Instance.GetSprite("effect_crit_chance"), effectLevel);
+                }
+            }
         }
-        
+
         public string Text()
         {
-            throw new System.NotImplementedException();
+            return $"You have exercised for <b>{Data.Sum(e => e.Duration.TotalMinutes)} minutes</b>. This gives you {Effect.Text()}.";
         }
 
         public string Description()
         {
-            throw new System.NotImplementedException();
+            return $"You have exercised for {Data.Sum(e => e.Duration.TotalMinutes)} minutes yesterday.";
         }
     }
 }
