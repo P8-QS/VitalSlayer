481df7c83a4de97217535334a489df489ba9c552
sabotack <sabotack.2001@gmail.com>
2025-04-25 14:52:52 +0200

    Add exercise metric to metricsmanager

--- Assets/Scripts/Managers/MetricsManager.cs
+++ Assets/Scripts/Managers/MetricsManager.cs
@@ -7,7 +7,7 @@ namespace Managers
     public class MetricsManager : MonoBehaviour
     {
         public static MetricsManager Instance { get; private set; }
-        public Dictionary<string, IMetric> metrics = new Dictionary<string, IMetric>();
+        public Dictionary<string, IMetric> metrics = new();
 
         public void Start()
         {
@@ -17,12 +17,12 @@ namespace Managers
 
                 var stepsMetric = new StepsMetric();
                 var sleepMetric = new SleepMetric();
-                // var exerciseMetric = new ExerciseMetric();
+                var exerciseMetric = new ExerciseMetric();
                 var screenTimeMetric = new ScreenTimeMetric();
 
                 if (stepsMetric.Data != null) metrics.Add(stepsMetric.Name, stepsMetric);
                 if (sleepMetric.Data != null) metrics.Add(sleepMetric.Name, sleepMetric);
-                // if (exerciseMetric.Data != null) metrics.Add(exerciseMetric.Name, exerciseMetric);
+                if (exerciseMetric.Data != null) metrics.Add(exerciseMetric.Name, exerciseMetric);
                 if (screenTimeMetric.Data != 0) metrics.Add(screenTimeMetric.Name, screenTimeMetric);
             }
             else
