f4a452bde4c54b1f90f7efa2a497527a5f94cb42
Mads Risager <madssr2@gmail.com>
2025-04-25 13:15:44 +0200

    Refactor state manager

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine.Serialization;
+
+
+[Serializable]
+public struct Perk
+{
+    public string name;
+    public int cost;
+    public int level;
+}
+
+[Serializable]
+public struct State
+{
+    public int experience;
+    public int points;
+    public List<Perk> perks;
+}
