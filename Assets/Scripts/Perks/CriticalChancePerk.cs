bfb92708126ccc85b176494fafb26c28ddf72d83
Mads Risager <madssr2@gmail.com>
2025-04-28 10:54:44 +0200

    Add crit and attack dmg perk

---
+++ Assets/Scripts/Perks/CriticalChancePerk.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+public class CriticalChancePerk : IPerk
+{
+    public string Name => "Critical Strike Chance";
+    public int Cost { get; set; } = 1;
+    public int Level { get; set; } = 0;
+
+    public string Description()
+    {
+        if (Level == 0)
+            return $"You will get <b>{GetMultiplier(Level + 1)}% increased</b> critical strike chance.";
+
+        return
+            $"You will get <b>{GetMultiplier(Level + 1)}% increased</b> critical strike chance if you upgrade. Currently you have <b>{GetMultiplier(Level)}%</b> increase.";
+    }
+
+    private int GetMultiplier(int level)
+    {
+        return level * 2;
+    }
+
+    public void Upgrade()
+    {
+        Level++;
+        Cost++;
+    }
+
+    public void Apply()
+    {
+        var baseCrit = GameManager.Instance.player.playerStats.critChance;
+        var multiplier = 1 + (GetMultiplier(Level) / 100.0);
+        float newCrit = (float)(baseCrit * multiplier);
+        GameManager.Instance.player.playerStats.critChance = newCrit;
+    }
+}
