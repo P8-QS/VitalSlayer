54ea16678aded1c33c4d8d8f6e4ec0f312b51005
Viktor Platz <viktorplatz@outlook.com>
2025-04-28 11:30:07 +0200

    folder specifically for helpers

---
+++ Assets/Scripts/Helpers/GameConstants.cs
@@ -0,0 +1,9 @@
+public class GameConstants
+{
+
+    // UI constants
+    public const int MIN_DAMAGE_FONT_SIZE = 6;
+    public const int MAX_DAMAGE_FONT_SIZE = 10;
+    public const int CRIT_FONT_SIZE_BONUS = 4;
+
+}
