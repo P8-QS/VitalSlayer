d32dd4bf5e3ebc2c52b1a9357cae4db99262347d
sabotack <sabotack.2001@gmail.com>
2025-04-29 08:25:52 +0200

    Add HeartRateVariability model

---
+++ Assets/Scripts/Data/Models/HeartRateVariabilityRmssdRecord.cs
@@ -0,0 +1,21 @@
+using System;
+using Newtonsoft.Json;
+
+namespace Data.Models
+{
+    [Serializable]
+    public class HeartRateVariabilityRmssdRecord
+    {
+        [JsonProperty("time")]
+        public DateTime Time { get; set; }
+        
+        [JsonProperty("zoneOffset")]
+        public string ZoneOffset { get; set; }
+        
+        [JsonProperty("heartRateVariabilityMillis")]
+        public decimal Energy { get; set; }
+        
+        [JsonProperty("metadata")]
+        public Metadata Metadata { get; set; }
+    }
+}
