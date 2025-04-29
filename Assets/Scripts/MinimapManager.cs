0b28025ee3727b8352ee394a30a26d454cc45b6f
Mads Risager <madssr2@gmail.com>
2025-04-29 10:40:17 +0200

    Fade out minimap when player is under

--- Assets/Scripts/MinimapManager.cs
+++ Assets/Scripts/MinimapManager.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using Dungeon;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MinimapManager : MonoBehaviour
 {
     public List<string> VisitedRooms = new();
 
     public static MinimapManager Instance;
+
+    public float visibleAlpha = 0.95f;
+    public float invisibleAlpha = 0.2f;
+
+    private float currentAlpha;
+    public float fadeSpeed = 5f; // Adjust for faster/slower fade
 
     private void Awake()
     {
@@ -31,6 +38,35 @@ public class MinimapManager : MonoBehaviour
         return rooms;
     }
 
+
+    public void Update()
+    {
+        var player = GameManager.Instance.player;
+
+        if (!Camera.main || !player) return;
+
+        var screenPoint = Camera.main.WorldToScreenPoint(player.transform.position);
+        var minimapRect = gameObject.GetComponent<RectTransform>();
+
+        // Get screen-space corners of the minimap UI
+        Vector3[] corners = new Vector3[4];
+        minimapRect.GetWorldCorners(corners);
+
+        Rect minimapScreenRect = new Rect(corners[0].x, corners[0].y,
+            corners[2].x - corners[0].x,
+            corners[2].y - corners[0].y);
+
+        bool playerOverlap = minimapScreenRect.Contains(screenPoint);
+
+        var canvas = gameObject.GetComponent<CanvasGroup>();
+
+        float targetAlpha = playerOverlap ? invisibleAlpha : visibleAlpha;
+
+        // Smoothly interpolate the alpha
+        currentAlpha = Mathf.Lerp(currentAlpha, targetAlpha, Time.deltaTime * fadeSpeed);
+        canvas.alpha = currentAlpha;
+    }
+
     public void UpdateMinimap(RoomInstance currentRoom)
     {
         VisitedRooms.Add(currentRoom.GameObject.name);
@@ -44,7 +80,7 @@ public class MinimapManager : MonoBehaviour
             if (fogController == null)
             {
                 continue;
-            };
+            }
 
             if (room.GameObject.name == currentRoom.GameObject.name)
             {
