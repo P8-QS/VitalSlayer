e6866d9c8957cea2d41c6de989758c3dbf44b433
Viktor Platz <viktorplatz@outlook.com>
2025-04-30 22:58:27 +0200

    CriticalChance Perk

--- Assets/Scripts/Perks/CriticalChancePerk.cs
+++ Assets/Scripts/Perks/CriticalChancePerk.cs
@@ -29,9 +29,14 @@ public class CriticalChancePerk : IPerk
 
     public void Apply()
     {
-        var baseCrit = GameManager.Instance.player.playerStats.critChance;
+        if (GameManager.Instance == null || GameManager.Instance.player == null ||
+            GameManager.Instance.player.playerStats == null)
+            return;
+
+        var player = GameManager.Instance.player;
+        var baseCrit = player.playerStats.CritChance;
         var multiplier = 1 + (GetMultiplier(Level) / 100.0);
         float newCrit = (float)(baseCrit * multiplier);
-        GameManager.Instance.player.playerStats.critChance = newCrit;
+        player.playerStats.CritChance = newCrit;
     }
 }
