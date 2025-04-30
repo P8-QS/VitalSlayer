4374b5ea3bc78ed3dffbb7da809718aef35749ec
Gustas Jucaitis <gustasjucaitis@gmail.com>
2025-04-30 09:24:59 +0200

    Adjusted levels of VO2 max metrics and DodgeSlimeAcidEffect to match

--- Assets/Scripts/Effects/DodgeSlimeAcidEffect.cs
+++ Assets/Scripts/Effects/DodgeSlimeAcidEffect.cs
@@ -5,7 +5,7 @@ namespace Effects{
     public class DodgeSlimeAcidEffect : IEffect
     {
         private int _level;
-        public static float SlowFactorEffect { get; private set; } = 0.5f;  //  default value         
+        public static float SlowFactorEffect { get; private set; } = 0.5f;  //  default value compared to slime puddle info.         
         public static bool puddleImmunityEffect {get; private set;} = false;  // default value
         private Sprite _icon;
         string IEffect.Name => "Dodge Slime Acid";
@@ -20,44 +20,32 @@ namespace Effects{
         }
 
         
-        string IEffect.Text()
+        string IEffect.Text() => Level switch
         {
-            return Level switch
-            {
-                1 => "<color=#EA2E1E>Level 1</color> - <color=#EA2E1E>25%</color>Extreme slime puddle slowdown.",
-                2 => "<color=#F5CC45>Level 2</color> - <color=#F5CC45>50%</color> Reduced slime puddle slowdown.",
-                3 => "<color=#3AE75B>Level 3</color> - <color=#3AE75B>No</color> No puddle slowdown or damage over time.",
-                _ => throw new System.ArgumentOutOfRangeException(nameof(Level), "Must be level 1, 2 or 3.")
-            };
-        }
-        
-        string IEffect.Description()
-        {
-            return "Depending on level, you will be slowed down by slime puddles more or less, you can also become immune to their damage.";
-        }
-        
+            0 => "<color=#EA2E1E>Level 0</color> - <color=#EA2E1E>25%</color> speed in slime puddles.",
+            1 => "<color=#F5CC45>Level 1</color> - <color=#F5CC45>50%</color> speed in slime puddles.",
+            2 => "<color=#3AE75B>Level 2</color> - <color=#3AE75B>Immune</color> to puddle slowdown and damage.",
+            _ => throw new System.ArgumentOutOfRangeException(nameof(Level), "Must be level 0, 1 or 2.")
+        };
+
+        string IEffect.Description() =>
+            "Reduces or removes slime puddle slowdown and damage based on level.";
+
         public void Apply()
         {
-            if(_level == 3){
-                //Good
-                //SlowFactorEffect = 1f;
+            switch (_level)
+            {
+            case 2:
                 puddleImmunityEffect = true;
-
-
-            }
-            else if(_level == 1){
-                //Bad
-                SlowFactorEffect = 0.25f;
-                puddleImmunityEffect = false;
-
-            }
-            else{
-                //Neutral (E.g. Lvl 2)
+                break;
+            case 1:
                 SlowFactorEffect = 0.5f;
                 puddleImmunityEffect = false;
-
-
-
+                break;
+            default:
+                SlowFactorEffect = 0.25f;
+                puddleImmunityEffect = false;
+                break;
             }
         }
     }
