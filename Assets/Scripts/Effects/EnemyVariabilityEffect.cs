7f462d60659b22083a81c746d51486d3051f0bc4
Mads Risager <madssr2@gmail.com>
2025-04-30 16:56:46 +0200

    Fix enemy variability text

--- Assets/Scripts/Effects/EnemyVariabilityEffect.cs
+++ Assets/Scripts/Effects/EnemyVariabilityEffect.cs
@@ -15,19 +15,25 @@ namespace Effects
             Icon = icon;
             Level = level;
         }
-        
+
         public string Text()
         {
-            return $"<color=#F5CC45>enemy variability level {Level}</color>";
+            return Level switch
+            {
+                1 => "<color=#EA2E1E>low enemy variability level</color>",
+                2 => "<color=#F5CC45>some enemy variability level</color>",
+                3 => "<color=#3AE75B>high enemy variability level</color>",
+                _ => throw new ArgumentOutOfRangeException()
+            };
         }
 
         public string Description()
         {
             return (HeartRateVariabilityMetric.Hrv)Level switch
             {
-                HeartRateVariabilityMetric.Hrv.Low => "Enemies have low variability",
-                HeartRateVariabilityMetric.Hrv.Normal => "Enemy variability is not affected",
-                HeartRateVariabilityMetric.Hrv.High => "Enemies have high variability",
+                HeartRateVariabilityMetric.Hrv.Low => "Enemies health and damage will have low variability.",
+                HeartRateVariabilityMetric.Hrv.Normal => "Enemy health and damage will have some variability.",
+                HeartRateVariabilityMetric.Hrv.High => "Enemies health and damage have high variability.",
                 _ => throw new ArgumentOutOfRangeException()
             };
         }
