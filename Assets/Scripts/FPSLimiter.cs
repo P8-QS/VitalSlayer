677b3ac05df835e132f522fd80b75c44dfd792cc
Mads Risager <madssr2@gmail.com>
2025-04-30 14:17:32 +0200

    Make singleton

--- Assets/Scripts/FPSLimiter.cs
+++ Assets/Scripts/FPSLimiter.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class FPSLimiter : MonoBehaviour
 {
+    private static FPSLimiter instance;
     private const double FallbackFPS = 60;
 
     private void Start()
     {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
         var resolutions = Screen.resolutions;
         var maxFPS = resolutions.Max(res => res.refreshRateRatio.value);
         var targetFPS = Math.Max(FallbackFPS, maxFPS);
-        
-        DontDestroyOnLoad(gameObject);
-        
+
+
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = (int)targetFPS;
         Debug.Log($"RefreshRateRatio: {maxFPS}");
