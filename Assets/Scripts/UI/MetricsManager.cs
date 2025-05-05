435e266345fb23588759d8fec73725e2b2a43bd5
Mads Risager <madssr2@gmail.com>
2025-05-05 15:07:12 +0200

    Show permission needed text when less than 2 metrics

--- Assets/Scripts/UI/MetricsManager.cs
+++ Assets/Scripts/UI/MetricsManager.cs
@@ -3,29 +3,32 @@ using UnityEngine;
 using TMPro;
 using Metrics;
 
-namespace UI {
+namespace UI
+{
     public class MetricsManagerUI : MonoBehaviour
     {
-        public GameObject metricCardPrefab;  // Assign the MetricCard prefab in Unity Inspector
-        public Transform parentPanel;  // The UI Panel that holds all metric cards
-        public Transform modalParent;  // The UI Panel that holds all modals
+        public GameObject metricCardPrefab; // Assign the MetricCard prefab in Unity Inspector
+        public Transform parentPanel; // The UI Panel that holds all metric cards
+        public Transform modalParent; // The UI Panel that holds all modals
 
         private void Start()
         {
             Debug.Log("MetricsManagerUI is running!");
 
             var metrics = MetricsManager.Instance.metrics.Values;
-            if (metrics.Count is 0) {
-                ShowErrorText("No metrics available.");
-            } else {
-                foreach (var metric in metrics) 
-                {
-                    AddMetric(metric);
-                }
+            if (metrics.Count < 2)
+            {
+                ShowErrorText("No metrics available. You need to allow QSCrawler to access your Health Connect data.");
+            }
+
+            foreach (var metric in metrics)
+            {
+                AddMetric(metric);
             }
         }
 
-        void AddMetric(IMetric metric) {
+        void AddMetric(IMetric metric)
+        {
             GameObject newCard = Instantiate(metricCardPrefab, parentPanel);
             MetricCardUI metricUI = newCard.GetComponent<MetricCardUI>();
             metricUI.modalParent = modalParent;
@@ -44,4 +47,3 @@ namespace UI {
         }
     }
 }
-
